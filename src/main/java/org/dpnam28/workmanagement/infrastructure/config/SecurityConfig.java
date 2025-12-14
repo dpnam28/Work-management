69b1c5b7494b3a197b5addabcb935df5142c5547
Duong Nam <dnam281224@gmail.com>
2025-12-15 00:39:15 +0700

    Swagger OpenAPI for documentation

--- src/main/java/org/dpnam28/workmanagement/infrastructure/config/SecurityConfig.java
+++ src/main/java/org/dpnam28/workmanagement/infrastructure/config/SecurityConfig.java
@@ -39,7 +39,12 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html")
+                        .permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
